fc58c5595d37def0c249000131609adc95506c0a
Dong Xiaoning <dongxn02@gmail.com>
2025-02-14 17:38:44 +0800

    add bash scripts: victim deepseek-r1

---
+++ scripts/victim-deepseek-r1.sh
@@ -0,0 +1,27 @@
+# SWQ (SATA-ELP)
+declare -a pss=('swq-mask-sw' 'swq-mask-sp' 'swq-mask-mw' 'swq-mask-mp')
+
+for ps in ${pss[@]};do
+    python src/main.py \
+        --input_dataset advbench-custom \
+        --victim_model_name deepseek-r1 \
+        --judge_model_name gpt-4o \
+        --ps ${ps} \
+        --model_name gpt-4o \
+        --mode 7 
+done
+
+
+
+# TextInfilling (SATA-MLM)
+declare -a pss=('wiki-text-infilling-sw' 'wiki-text-infilling-sp' 'wiki-text-infilling-mw' 'wiki-text-infilling-mp')
+
+for ps in ${pss[@]};do
+    python src/main.py \
+        --input_dataset advbench-custom \
+        --victim_model_name deepseek-r1 \
+        --judge_model_name gpt-4o \
+        --ps ${ps} \
+        --model_name gpt-4o \
+        --mode 7 
+done
